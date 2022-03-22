c23c8db1f94246daa7ff3e5a9ead2a943e6a3e67
Kleber Alves <kleber@pop-os.localdomain>
2022-03-22 20:55:23 -0300

    Removendo teste de permissões.

--- docker/dspace/build-dspace.sh
+++ docker/dspace/build-dspace.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo docker rm -f dspace_miguilim
-sudo docker rmi -f $(docker images --filter=reference='mrgurgel/dspace:*' --format "{{.ID}}")
-sudo docker-compose -f ./docker-compose.yml up -d dspace
+docker rm -f dspace_miguilim
+docker rmi -f $(docker images --filter=reference='mrgurgel/dspace:*' --format "{{.ID}}")
+docker-compose -f ./docker-compose.yml up -d dspace
