b4a77fab0579e368eff365bc64a5ac30dd9ec0c4
Kleber Alves <kleber@pop-os.localdomain>
2022-03-22 21:01:10 -0300

    Ajustando remoção de imagem.

--- docker/dspace/build-dspace.sh
+++ docker/dspace/build-dspace.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker rm -f dspace_miguilim
-docker rmi -f $(docker images --filter=reference='mrgurgel/dspace:*' --format "{{.ID}}")
+docker rmi -f mrgurgel/dspace_miguilim
 docker-compose -f ./docker-compose.yml up -d dspace
