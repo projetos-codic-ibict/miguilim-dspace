db0d061bc1265c991eb6f9672cbc38c57bc04156
Kleber Alves <kleber@pop-os.localdomain>
2022-03-27 02:17:17 -0300

    Ajuste da rede no container dspace.

--- docker/dspace/build-dspace.sh
+++ docker/dspace/build-dspace.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-docker rm -f dspace_miguilim
-#docker rmi -f $(docker images --filter=reference='mrgurgel/dspace:*' --format "{{.ID}}")
-#docker rmi -f mrgurgel/dspace_miguilim
-docker-compose -f ./docker-compose.yml up -d dspace
+docker-compose up -d --build
